784cadf8d03a113bc997ad2590acf0bfa89eb68a
Dmitry <dmitry.dima-sol@yandex.ru>
2025-02-19 23:38:21 +0300

    add base files + first template
    hello commit

---
+++ android/app/src/main/kotlin/com/example/flutter_templates/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_templates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 48, 127, 141),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Название + слоган
+              Container(
+                child: Column(
+                  children: const [
+                    Text('medinow', style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Rubik',
+                    ),),
+                    Text('Meditate with us!', style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 70),
+
+              // Кнопочки
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 52),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text("Sign in with Apple", style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 180, 218, 224),
+                        minimumSize: const Size(double.infinity, 52),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text("Continue with Email or Phone", style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ),
+                    const SizedBox(height: 8),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Continue with Google", style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),),
+                      
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              // Картиночка
+              SizedBox(
+                height: 350,
+                child: Image.asset("assets/images/meditate-image.png")
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
